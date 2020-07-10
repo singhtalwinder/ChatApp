b62b524b6369d7ab883b537841fe27e33d889439
Talwinder Singh <singhtalwinder790@gmail.com>
2020-07-10 17:12:51 +0530

    added createUser contoller and validation

---
+++ controller/uservalidation.js
@@ -0,0 +1,14 @@
+import Joi from "@hapi/joi";
+
+export default {
+	createUser: (data) => {
+		const schema = Joi.object({
+			fname: Joi.string().min(1).max(20).required(),
+			lname: Joi.string().min(1).max(20).required(),
+			email: Joi.string().min(6).max(40).required().email(),
+			password: Joi.string().min(6).max(20).required(),
+			confirmPassword: Joi.string(),
+		});
+		return schema.validate(data);
+	},
+};
