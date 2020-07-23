98c9266cf81acef343a49dca604a103fdd1541be
Talwinder Singh <singhtalwinder790@gmail.com>
2020-07-23 15:22:35 +0530

    Added error message

--- view/src/components/IconInput.js
+++ view/src/components/IconInput.js
@@ -1,31 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import "./IconInput.css";
 
 function IconInput(props) {
-	const handleFocus = (event) => {
-		const iconInput = event.target.parentNode;
-		iconInput.style.borderColor = "black";
+	const [borderColor, setBorderColor] = useState(() => {
+		if (props.error) {
+			return "red";
+		}
+		return "#0084ff";
+	});
+	const handleFocus = () => {
+		setBorderColor("black");
 	};
 
-	const handleBlur = (event) => {
-		const iconInput = event.target.parentNode;
-		iconInput.style.borderColor = "#0084FF";
+	const handleBlur = () => {
+		if (!props.error) {
+			setBorderColor("#0084ff");
+		} else {
+			setBorderColor("red");
+		}
 	};
 
 	return (
-		<div className="icon-input">
-			<span>
-				<i className={props.icon}></i>
-			</span>
-			<input
-				type={props.type}
-				name={props.name}
-				placeholder={props.placeholder}
-				onChange={props.onChange}
-				onFocus={handleFocus}
-				onBlur={handleBlur}
-			/>
-			{props.endIcon && <props.endIcon />}
+		<div className="icon-input-wrapper">
+			<div className="icon-input" style={{ borderColor: borderColor }}>
+				<span>
+					<i className={props.icon}></i>
+				</span>
+				<input
+					type={props.type}
+					name={props.name}
+					placeholder={props.placeholder}
+					onChange={props.onChange}
+					onFocus={handleFocus}
+					onBlur={handleBlur}
+				/>
+				{props.endIcon && <props.endIcon />}
+			</div>
+			{props.error && <p className="error-box">{"*" + props.errorMessage}</p>}
 		</div>
 	);
 }
