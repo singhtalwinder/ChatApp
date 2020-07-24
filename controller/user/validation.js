96080bb7e9e4f3a493361f233767c97b24c0b5da
Talwinder Singh <singhtalwinder790@gmail.com>
2020-07-24 14:41:22 +0530

    Changed signup validation

--- controller/user/validation.js
+++ controller/user/validation.js
@@ -7,7 +7,7 @@ export default {
 			lname: Joi.string().min(1).max(20).required(),
 			email: Joi.string().max(40).required().email(),
 			password: Joi.string().min(6).max(20).required(),
-			confirmPassword: Joi.string(),
+			confirmPassword: Joi.string().valid(data.password),
 		});
 		return schema.validate(data);
 	},
