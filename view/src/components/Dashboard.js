7dd418909ef95ad8006cd8bd842f886bd289eabb
Talwinder Singh <singhtalwinder790@gmail.com>
2020-07-25 15:26:33 +0530

    Added UI to show online users with fake data

--- view/src/components/Dashboard.js
+++ view/src/components/Dashboard.js
@@ -1,4 +1,6 @@
-import React, { Fragment, useEffect } from "react";
+import React, { useEffect } from "react";
+import { onlineUsers } from "./fakeData";
+import "./Dashboard.css";
 
 const Dashboard = (props) => {
 	const signout = () => {
@@ -14,10 +16,25 @@ const Dashboard = (props) => {
 	}, []);
 
 	return (
-		<Fragment>
-			<h1>Welcome</h1>
-			<button onClick={signout}>SignOut</button>
-		</Fragment>
+		<div className="dashboard-container">
+			<div className="online-users">
+				<h4>Online users</h4>
+				<div>
+					{onlineUsers.map((onlineUser) => (
+						<div className="online-user">
+							<span className="initials">
+								{onlineUser.fname[0].toUpperCase() +
+									onlineUser.lname[0].toUpperCase()}
+							</span>
+							<span className="full-name">
+								{onlineUser.fname + " " + onlineUser.lname}
+							</span>
+						</div>
+					))}
+				</div>
+			</div>
+			<div className="chat-box">chat-box</div>
+		</div>
 	);
 };
 
